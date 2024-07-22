804a7538e0b2d1bca023c873684dce3302c39af9
rizvan008 <rizvan008@yahoo.com>
2024-07-22 01:42:59 -0700

    first version

--- script.js
+++ script.js
@@ -1,61 +1,78 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import gsap from 'gsap';
-//**canvas for the 3d scene visuals
-const canvas = document.querySelector('canvas.webgl');
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import gsap from "gsap";
 
-//**required size for the 3d scene in the web page 
-let size = {width: 800,hieght: 600};
+//**canvas for the 3d scene visuals */
+const canvas = document.querySelector("canvas.webgl");
+
+//**required size for the 3d scene in the web page */
+let size = { width: 800, height: 600 };
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60,size.width/size.hieght,1,100);
-camera.position.set(2,4,3);
+const camera = new THREE.PerspectiveCamera(
+  60,
+  size.width / size.height,
+  1,
+  100
+);
+
+camera.position.set(2, 4, 5);
 scene.add(camera);
 
-// **camera control behaviour
+// **camera control behavior */
 // const control = new THREE.OrbitControls(camera,canvas);
 // scene.add(control);
 
-// **creating 3d objects in the screen
-const shape = new THREE.BoxGeometry(1,1,1);
-const cover = new THREE.MeshBasicMaterial({color:""});
-//**colour manipulation options
-// cover.color = new THREE.Color(0xff00ff);
-cover.color = new THREE.Color('skyblue');
+// **creating 3d objects in the screen */
+const shape = new THREE.BoxGeometry(1, 1, 1);
+const cover = new THREE.MeshBasicMaterial({ color: "" });
 
+//**color manipulation options */
+cover.color = new THREE.Color("skyblue");
+// cover.color = new THREE.Color(0xff00ff);
 // cover.color = new THREE.Color('rgb(250,0,0)');
 // cover.color = new THREE.Color('hsl(95, 100%, 40%)');
-const square = new THREE.Mesh(shape,cover);
-camera.lookAt(square.position);
-square.position.z=5;
 
-//**bundling requierd object together
+const square = new THREE.Mesh(shape, cover);
+square.material.wireframe = true;
+square.position.z = 3;
+
+//**creating required objects together */
 const cubes = [];
-const group = new THREE.Group();
-scene.add(group, square);
 
-function creat(x,color,y,z ) {
-const cover1 = new THREE.MeshBasicMaterial();
-    const cube = new THREE.Mesh(shape,cover1);
-    cube.position.set(x,y,z);
+function create(x, color, y, z) {
+    const cover1 = new THREE.MeshBasicMaterial();
+    const cube = new THREE.Mesh(shape, cover1);
+    cube.position.set(x, y, z);
     cover1.color = new THREE.Color(color);
-    cubes.push (cube);
+    cubes.push(cube);
     return cube;
 }
 
-group.add(creat(1.5,'red',.5),creat(-.5,'yellow',0),creat(-1.5,'green',-.5),creat(.50,'blue',0))
-// group.add(creat(2,0xff0000),creat(-2,0x0000ff),creat(0,0x00ff00))
+//**bundling required object together */
+const group = new THREE.Group();
 
-// **enlarging the size of objects
-group.scale.set(1,2,0.5)
+group.add(
+  create(1.5, "red", 0.5),
+  create(-0.5, "yellow", 0),
+  create(-1.5, "green", -0.5),
+  create(0.5, "blue", 0)
+);
 
-// **rotating the objects in their axis uing Euler
-// group.rotateY (3.14*1)
-group.rotation.y = 3.14
-// group.rotation.z = 3.14
-// group.rotation.x = 3.14
+scene.add(group, square);
+console.log(group);
 
-// **Example of rotation using Quaternion
+// **enlarging size of objects */
+group.scale.set(1, 2, 0.5);
+
+// **rotating the objects in their axis using Euler */
+group.rotateY (3.14 * 1);
+// group.rotateZ (3.14);
+// group.rotation.y = 3.14;
+// group.rotation.z = Math.PI;
+// group.rotation.x = 3.14 * 1;
+
+// **Example of rotation using Quaternion */
 // const quaternion = new THREE.Quaternion();
 //   quaternion.multiply(new THREE.Quaternion(0,1,0, 0)); // Rotate around y-axis
 //   group.quaternion.copy(quaternion);
@@ -63,33 +80,39 @@ group.rotation.y = 3.14
 //** find the measurements in between objects */
 console.log(group.position.distanceTo(camera.position));
 console.log(square.position.distanceTo(camera.position));
+
 console.log(square.position.normalize());
 console.log(cubes);
 
+//** projecting the visuals in the web page */
 
-//**renderer/ project the vissuals in the web page
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(size.width, size.height);
 
-const renderer = new THREE.WebGLRenderer({canvas: canvas});
-renderer.setSize(size.width,size.hieght);
-
-square.material.wireframe = true;
 const time = new THREE.Clock();
 
 /** creating motions to objects */
-function ani() {
-    
-    gsap.to(group?.position,{duration: 5, delay:1, x:2});
-    gsap.to(group?.position,{duration:5, delay:3, x:0});
+try {
+  function tick() {
+    gsap.to(group?.position, { duration: 5, delay: 1, x: 2 });
+    gsap.to(group.position, { duration: 5, delay: 3, x: 0 });
+  }
+  tick();
+} catch (error) {
+  console.log(error);
 }
-ani()
+camera.lookAt(group.position);
 
 const animate = () => {
-        group.rotateZ((Math.PI*(0.01)));
-        const tic = time.getElapsedTime()
-        square.position.y = Math.cos(tic)
-        square.position.x = Math.sin(tic)*Math.cos(tic)
-        renderer.render(scene,camera);
-        window.requestAnimationFrame(animate)
-    }
-    
-    animate();
+  // group.rotateZ(Math.PI * 0.01);
+  group.rotation.z+=(Math.PI * 0.01);
+  
+  const seconds = time.getElapsedTime();
+  square.position.y = Math.cos(seconds)*2;
+  square.position.x = Math.sin(seconds) * Math.cos(seconds)*2;
+  // camera.lookAt(square.position);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+};
+
+animate();
