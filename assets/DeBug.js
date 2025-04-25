5de47339743ec7bae2da9376b3f5e7a1c85393cc
rizvan008 <rizvan008@yahoo.com>
2025-04-25 20:59:36 +0530

    vite CJS Node API deprecated in version 6 fixed 	modified:   package.json

--- assets/DeBug.js
+++ assets/DeBug.js
@@ -1,27 +1,27 @@
 import tank,{frontRightTyre, frontLeftTyre} from "./tank.js";
 import { yard, containers, ground } from "./containers.js"
-import { lifter } from "./lift.js";
+import lifter from "./lift.js";
 import GSAP from "gsap";
 import * as dat from "dat.gui";
 import { scene } from "./scene.js";
 
-export const gs_p = new dat.GUI();
+export const DAT = new dat.GUI();
 
 /** creating motions to objects */
 function tick() {
-gs_p.add(yard.position, 'x').min(0).max(10).step(0.01).name('yard x-axis')
-gs_p.add(yard.position, 'y').min(0).max(10).step(0.01).name('yard y-axis')
-gs_p.add(yard, 'visible').name('yard visible')
-gs_p.add(frontRightTyre.material, 'wireframe').name('front Right tyre Wireframe')
-const folder = gs_p.addFolder("lifter")
+DAT.add(yard.position, 'x').min(0).max(10).step(0.01).name('yard x-axis')
+DAT.add(yard.position, 'y').min(0).max(10).step(0.01).name('yard y-axis')
+DAT.add(yard, 'visible').name('yard visible')
+DAT.add(frontRightTyre.material, 'wireframe').name('front Right tyre Wireframe')
+const folder = DAT.addFolder("lifter")
 folder.add(lifter, 'visible').name('lifter visible")')
 folder.add(lifter.position, 'x').min(0).max(10).step(0.01)
 folder.add(lifter.position, 'y').min(0).max(10).step(0.01)
 folder.addColor(lifter.children[0].material, 'color').onChange((color) => { color = new THREE.Color('red') })
 folder.add(lifter.children[0].material, 'side')
 folder.add(lifter.children[0].material, 'wireframe')
-folder.add(tank.children[tank.children.length].position, 'y').min(0).max(10).step(0.01)
-folder.add(tank.children[tank.children.length].position, 'x').min(0).max(10).step(0.01)
+// folder.add(tank.children[tank.children.length].position, 'y').min(0).max(10).step(0.01)
+// folder.add(tank.children[tank.children.length].position, 'x').min(0).max(10).step(0.01)
 folder.open();
 
 }
@@ -31,5 +31,5 @@ folder.open();
       tick();
     }
   } catch (error) {
-    console.error('error in gs_p, debug.js : ', error);
+    console.error('error in DAT, debug.js : ', error);
   }
