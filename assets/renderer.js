9c2f779f247de0a123e3488a12618631dc88b136
rizvan008 <rizvan008@yahoo.com>
2025-04-25 13:35:17 -0400

    ondemand camera scene modified:   .gitignore modified:   assets/renderer.js

--- assets/renderer.js
+++ assets/renderer.js
@@ -1,4 +1,7 @@
 import * as THREE from "three"
+import {scene} from './scene.js';
+import { camera, debug_camera } from './camera.js';
+
 
 //**size of the 3d scene in the web page */
 export let size = { width: 800, height: 600 }; //update the size of the 3d scene in the web page
@@ -12,3 +15,12 @@ renderer.name = "renderer";
 //**required size for the 3d scene in the web page */
 renderer.setSize( size.width, size.height );
 renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2) );
+
+export let render = () =>{  renderer.render(scene, camera)};
+document.addEventListener( 'keypress', (e) => {
+    if (e.key === 'd'){
+    render =   renderer.render(scene, debug_camera)};
+    if (e.key === 'n'){
+    render =   renderer.render(scene, camera)};
+});
+
