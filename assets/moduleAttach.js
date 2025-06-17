231de7ac83428ddda2dfbcdd09dde00972ef8ada
rizvan008 <rizvan008@yahoo.com>
2025-06-17 15:18:03 -0400

    smooth animation back  modified:   assets/moduleAttach.js

--- assets/moduleAttach.js
+++ assets/moduleAttach.js
@@ -12,8 +12,7 @@ if(tank.position.distanceTo(lifter.position) < 5 && lifter.userData.attachable){
         keyPressHandler = (evn) => {
             if(evn.key == 'r'){
                 tank.userData.attachable ? moveTillAttach(tank, lifter) : undefined; // this will move the tank to the lift module position
-            }
-        }
+            }}
         document.addEventListener('keypress', keyPressHandler )
     }
 }else{
@@ -25,54 +24,48 @@ if(tank.position.distanceTo(lifter.position) < 5 && lifter.userData.attachable){
 }
 //write detaching logic */
 }
-function findLocation(module, car){
+function findLocation(car, module){
     if (module.position.x > car.position.x){
         'module in positive x quadrant'
     }
     if (module.position.z > car.position.z){
         'module in positive z quadrant'
-    }
-    
+    }    
 }
-
 // grab location and check whether need to move, create moving logic*
 export function moveTillAttach(car, module){
-    // Check if the car is already at the module's position
     const carPosition = car.position.clone();
     const modulePosition = module.position.clone();
+// Check if the car is already at the module's position
     if( carPosition.equals(modulePosition)) { // validate whether car and module are in same position
         // module.parent.remove(scene.getObjectByName('GridHelper'));
         // module.removeFromParent();
         (scene.getObjectByName('tank').add(module));
         module.userData.attachable = false;
         car.userData.attachable = false;
-        console.log('movetill part :',scene);
+        console.log(' Equals part :',scene);
     }else {
         const distance = (carPosition.distanceTo(modulePosition));
         console.log('distance: ', distance);
-        const direction = modulePosition.sub(carPosition).normalize();
+        const direction = modulePosition.sub(carPosition);
         console.log('direction: ', direction.toArray());
+        // direction value never be exact zero, so we need to use normalized vector
+        let direction_normalized = direction.normalize();
         const speed = 0.1; // Adjust the movement speed as needed
         const miniSpeedToMove = Math.min(speed, distance);
         
-        //move till target location, attach module,
+        //move till target location & attach to module,
         moveTo_target( car, module, direction, miniSpeedToMove);
+        
     }
 }
+
 function moveTo_target(car, module, direction, miniSpeedToMove) {
-
-    if(miniSpeedToMove <= 0) {
-        // module.clear();
-        // module.removeFromParent();
-        console.log('move target part : ', scene);
-
-        return; // Stop if the target is reached
-        }
 //moving logic applied to car
-    if(module) car.position.add(direction.multiplyScalar(miniSpeedToMove));
-
+car.position.add(direction.multiplyScalar(miniSpeedToMove));
+console.log('movetill LOOP part :',scene);
 // animation till module attachment
-    // window.requestAnimationFrame(() => moveTillAttach(car, module))
-    moveTillAttach(car, module)
+    window.requestAnimationFrame(() => moveTillAttach(car, module))
 }
+
 export default moduleAttach;
